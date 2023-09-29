6e735f93a00a9c3aa2a8255f4933d89d45f1178a
Avula Nagaraja <anagarajasfdc@gmail.com>
2023-09-29 16:04:37 +0530

    Recaptch validation

--- script.js
+++ script.js
@@ -11,20 +11,25 @@ function beforesubmit(event){
     let formatteddate = new Date(inputdate.value).toLocaleDateString("en-IN");
     outputdate.value = formatteddate; 
 	} else {
-    alert("please check the reCAPTCHA box to submit the form");
+    alert("please check the reCAPTCHA box to submit the Lead");
     event.preventDefault();
-  } 
-}
-function timestamp() { 
-    var response = document.getElementById("g-recaptcha-response");
-     if (response == null || response.value.trim() == "") {
-     var elems = JSON.parse(document.getElementsByName("captcha_settings")[0].value);
-     console.log('Elems->',elems  +"Captcha Settings--->",document.getElementsByName("captcha_settings")[0].value);
-     elems["ts"] = JSON.stringify(new Date().getTime());
-     document.getElementsByName("captcha_settings")[0].value = JSON.stringify(elems);
-     }
-     }
-    setInterval(timestamp, 500); 
+  }
+   
+ }
+
+   function timestamp() {
+       var response = document.getElementById("g-recaptcha-response");
+       if (response == null || response.value.trim() == "") 
+       {
+        var elems = JSON.parse(document.getElementsByName("captcha_settings")[0].value);
+        console.log('Elems->',elems  +"Captcha Settings--->",document.getElementsByName("captcha_settings")[0].value);
+        elems["ts"] = JSON.stringify(new Date().getTime());
+        document.getElementsByName("captcha_settings")[0].value = JSON.stringify(elems);
+       }
+       }
+        setInterval(timestamp, 500); 
+      
+
 function captchasuccess() {
   captchachecked = true;
 }
